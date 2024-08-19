9126ae7c06b457dc5ee452f2dd6fae43b6c364f2
Diego Gervasio <diegox300@gmail.com>
2024-08-19 16:56:55 -0300

    refactor: code structure.

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Ok");
+app.get('/', (req, res) => {
+  res.status(200).send('Ok');
 });
 
 export default app;
