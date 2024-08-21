ad54f7e48af5c4ee9e6fbc3eeedffd4da770bbc1
Diego Gervasio <diegox300@gmail.com>
2024-08-21 11:58:29 -0300

    chore: add role eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,38 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
 
+export default {
+  files: ['**/*.{js,mjs,cjs,ts}'],
+  languageOptions: {
+    globals: globals.browser, // Define os globals conforme necessário
+  },
+  plugins: {
+    // Adicione plugins se necessário
+    'eslint-plugin': pluginJs,
+    '@typescript-eslint': tseslint,
+  },
+  rules: {
+    // Regras específicas do plugin JavaScript
+    ...pluginJs.configs.recommended.rules,
+    // Regras recomendadas do TypeScript
+    ...tseslint.configs.recommended.rules,
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+    // Suas regras personalizadas
+    'no-console': 'warn', // Exemplo: Avisar sobre o uso de console.log
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Exemplo: Desabilitar a exigência de tipos explícitos em funções
+    "prettier/prettier": [
+      "error",
+      {
+        "printWidth": 80,
+        "tabWidth": 2,
+        "singleQuote": true,
+        "trailingComma": "all",
+        "arrowParens": "always",
+        "semi": false
+      }
+    ],
+    "camelcase": "off",
+    "no-useless-constructor": "off"
+  },
+};
