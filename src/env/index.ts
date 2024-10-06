be6b92b0b565ffe9883699263bbf0e334386796a
diegox300 <diegox300@gmail.com>
2024-10-06 18:49:39 -0300

    test: fix test

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,23 @@
-import 'dotenv/config' // Load environment variables from a .env file
-
-import { z } from 'zod' // Importing zod for schema validation
+import { z } from 'zod'
 
 // Define a schema for validating environment variables
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production']).default('development'), // Environment should be either 'development' or 'production', default is 'development'
-  PORT: z.coerce.number().default(3000), // Port should be a number, default is 3000
-  MONGO_URI: z.string().default('mongodb://localhost:27017/test'), // MongoDB URI should be a string, default is set for local development
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'), // Environment should be either 'development', 'production', or 'test', default is 'development'
+  PORT: z.coerce.number().default(8000), // Port should be a number, default is 8000
+  MONGO_URI: z
+    .string()
+    .url()
+    .default('mongodb://localhost:27017/tech-challenge-2'), // MongoDB URI should be a string and a valid URL
 })
 
 // Validate the environment variables against the schema
 const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-  throw new Error('Invalid environment variables') // Throw an error if the validation fails
+  console.error(_env.error.format()) // Log the validation errors for debugging
+  throw new Error('Invalid environment variables')
 }
 
 // Export the validated environment variables
