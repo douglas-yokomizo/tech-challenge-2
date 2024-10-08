6fce1c4a10ae561761d0d5c625aac841f83258aa
diegox300 <diegox300@gmail.com>
2024-10-08 13:47:03 -0300

    fix: test get-all-posts

--- src/tests/http/controllers/post/get-all-posts.test.ts
+++ src/tests/http/controllers/post/get-all-posts.test.ts
@@ -34,11 +34,13 @@ describe('Get All Posts', () => {
     const post1 = new Post({
       title: 'Test Post 1',
       content: 'This is test post 1',
-    }).save()
+    })
     const post2 = new Post({
       title: 'Test Post 2',
       content: 'This is test post 2',
-    }).save()
+    })
+    await post1.save()
+    await post2.save()
 
     const res = await request(app).get('/posts')
 
